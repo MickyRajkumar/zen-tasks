ad2b0fd90e0952818ecaed6f759b91f71aa7059c
Micky Rajkumar <micky.rajkumar@laitkor.com>
2025-02-24 17:51:56 +0530

    update .db file path

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 import os
 
-DB_PATH = os.path.expanduser("./task_reminder.db")
+db_folder = os.path.join(os.getenv("APPDATA"), "TaskCLI")
+os.makedirs(db_folder, exist_ok=True)
+DB_PATH = os.path.join(db_folder, "task_reminder.db")
