2b73b9739bc322b9113b181c0821996e422ef7b3
Micky Rajkumar <pathourajkumar@gmail.com>
2025-03-04 17:26:06 +0000

    fixed complete task issue

--- config.py
+++ config.py
@@ -1,3 +1,19 @@
 import os
+import sys
 
-DB_PATH = os.path.expanduser("./task_reminder.db")
+if sys.platform == "win32":
+    db_folder = os.path.join(os.getenv("APPDATA"), "TaskCLI")
+elif sys.platform == "linux":
+    xdg_data_home = os.getenv("XDG_DATA_HOME")
+    if xdg_data_home:
+        db_folder = os.path.join(xdg_data_home, "TaskCLI")
+    else:
+        db_folder = os.path.join(os.path.expanduser("~"), ".local", "share", "TaskCLI")
+else:
+    db_folder = os.path.join(os.path.expanduser("~"), ".local", "share", "TaskCLI")
+
+# Ensure the directory exists
+os.makedirs(db_folder, exist_ok=True)
+
+# Set the database path
+DB_PATH = os.path.join(db_folder, "task_reminder.db")
